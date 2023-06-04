a7b572ffaf7b14166a06246ffd48f192b7ef291f
aldinnasrunm <adnzain40@gmail.com>
2023-06-04 11:30:33 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./dist/script/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#FD636B",
+        primary: "#0c1117",
+        secondary: "#FD636B",
+      },
+      screens: {
+        "2xl": "1439px",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "3rem",
+        xl: "6rem",
+      },
+    },
+  },
+  plugins: [],
+}
+
